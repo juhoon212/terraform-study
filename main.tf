5527b6bd7ba352896f7b3c2309e42cb16d228d0d
juhoon212 <ws5501@naver.com>
2024-11-10 18:06:58 +0900

    data

--- main.tf
+++ main.tf
@@ -16,16 +16,28 @@ resource "aws_vpc" "default" {
   cidr_block = "10.0.0.0/16"
 
   tags = {
-    Name = "main"
+    Name = "tf_study_vpc"
   }
 }
 
 resource "aws_subnet" "public_subnet_1" {
   vpc_id     = aws_vpc.default.id
   cidr_block = "10.0.0.0/24"
+
+  tags = {
+    Name = "tf_study_public_subnet_1"
+  }
 }
 
-resource "aws_subnet" "private_subnet_1" {
+/*resource "aws_subnet" "private_subnet_1" {
   vpc_id     = aws_vpc.default.id
-  cidr_block = "10.0.1.0/24"
-}
+  cidr_block = "10.0.10.0/24"
+  tags = {
+    Name = "tf_study_private_subnet_1"
+  }
+}*/
+
+// 코드로 관리하지 않을 데이터들은 data 키워드를 붙여서 aws web에 있는 데이터와 동기화 시킬 수 있다.
+/*data "aws_subnet" "selected" {
+  id = "subnet_id"
+}*/
